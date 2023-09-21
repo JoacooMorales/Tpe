88ad93c640b003c9e01c9ff1605514ca4e40c65c
JoacooMorales <144643449+JoacooMorales@users.noreply.github.com>
2023-09-20 23:23:14 -0300

    Codigo SQL generado por la BBDD

---
+++ tpeweb2musica.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-09-2023 a las 04:21:59
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tpeweb2musica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artistas`
+--
+
+CREATE TABLE `artistas` (
+  `ID` int(11) NOT NULL,
+  `Nombre del Artista` varchar(255) NOT NULL,
+  `País de Origen` varchar(255) NOT NULL,
+  `Género Musical` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `canciones`
+--
+
+CREATE TABLE `canciones` (
+  `ID` int(11) NOT NULL,
+  `Título de la Canción` varchar(255) NOT NULL,
+  `Duración` time NOT NULL,
+  `Álbum ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `álbumes`
+--
+
+CREATE TABLE `álbumes` (
+  `ID` int(11) NOT NULL,
+  `Título del Álbum` varchar(255) NOT NULL,
+  `Año de Lanzamiento` int(11) NOT NULL,
+  `Artista ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `artistas`
+--
+ALTER TABLE `artistas`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  ADD KEY `FK_ID_AlbumesID` (`Álbum ID`);
+
+--
+-- Indices de la tabla `álbumes`
+--
+ALTER TABLE `álbumes`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_ArtistaID` (`Artista ID`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `canciones`
+--
+ALTER TABLE `canciones`
+  ADD CONSTRAINT `FK_ID_AlbumesID` FOREIGN KEY (`Álbum ID`) REFERENCES `álbumes` (`ID`);
+
+--
+-- Filtros para la tabla `álbumes`
+--
+ALTER TABLE `álbumes`
+  ADD CONSTRAINT `FK_ArtistaID` FOREIGN KEY (`Artista ID`) REFERENCES `artistas` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
